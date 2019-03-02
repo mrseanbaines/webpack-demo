ac92ac46c3ccec352557a6dbdfbbd48faa498cf6
Sean Baines <mrseanbaines@gmail.com>
2019-03-02 09:34:51 +0000

    Utilise tree shaking

--- src/index.js
+++ src/index.js
@@ -1,30 +1,13 @@
-import _ from 'lodash';
-import printMe from './print.js';
-import './styles.css';
-
-let element;
+import { cube } from './math.js';
 
 function component() {
-  element = document.createElement('div');
-  let btn = document.createElement('button');
+  let element = document.createElement('pre');
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
+  element.innerHTML = ['Hello webpack!', '5 cubed is equal to ' + cube(5)].join(
+    '\n\n'
+  );
 
   return element;
 }
 
 document.body.appendChild(component());
-
-if (module.hot) {
-  module.hot.accept('./print.js', function() {
-    console.log('Accepting the updated printMe module!');
-    document.body.removeChild(element);
-    element = component(); // Re-render the "component" to update the click handler
-    document.body.appendChild(element);
-  });
-}
