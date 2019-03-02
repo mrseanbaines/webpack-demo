043996375245d57db3ba152b7dd7652d91a71a80
Sean Baines <mrseanbaines@gmail.com>
2019-03-02 12:00:13 +0000

    Split environment configs

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import { cube } from './math.js';
 
+if (process.env.NODE_ENV !== 'production') {
+  console.log('Looks like we are in development mode!');
+}
+
 function component() {
   let element = document.createElement('pre');
 
-  element.innerHTML = ['Hello webpack!', '5 cubed is equal to ' + cube(5)].join(
+  element.innerHTML = ['Hello world!', '5 cubed is equal to ' + cube(5)].join(
     '\n\n'
   );
 
